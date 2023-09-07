77ef36fe74114fbe53ee4adc5104e4423fb1aa15
Anish Das <98608261+Anish-Das-GH@users.noreply.github.com>
2023-09-07 12:14:20 +0530

    v1.3

--- prime.sh
+++ prime.sh
@@ -2,11 +2,12 @@ echo "Enter a no"
 read num
 for((i=2; i<=num/2; i++))
 do
-	  if [ $((num%i)) -eq 0 ]
-		    then
-			        echo "$num is not a prime number."
-				    exit
-				      fi
-			      done
-			      echo "$num is a prime number."
-
+if [ $((num % i)) == 0 ]
+then
+    echo "NOT PRIME"
+    exit
+else
+    echo prime
+    exit
+fi
+done
